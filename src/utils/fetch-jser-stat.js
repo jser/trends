9538093f75a7bf6213900a70ffaaa0df9d1c01aa
azu <azuciao@gmail.com>
2018-05-15 19:52:07 +0900

    refactor: user @jser/data-fetcher

--- src/utils/fetch-jser-stat.js
+++ src/utils/fetch-jser-stat.js
@@ -1,48 +1,23 @@
 // LICENSE : MIT
 "use strict";
-import {JSerStat} from "jser-stat";
-function fetchURL(URL) {
-    return new Promise(function (resolve, reject) {
-        var req = new XMLHttpRequest();
-        req.open('GET', URL);
-        req.onload = function () {
-            if (req.status >= 200 && req.status < 300) {
-                resolve(JSON.parse(req.response));
-            } else {
-                reject(Error(req.statusText));
-            }
-        };
-        req.onerror = function () {
-            reject(Error(req.statusText));
-        };
-        req.send();
-    });
-}
+import { JSerStat } from "@jser/stat";
+import { fetchItems, fetchPosts } from "@jser/data-fetcher";
+
 function fetchData() {
-    if (process.env.NODE_ENV === 'development') {
-        return new Promise((resolve) => {
-            resolve([
-                require("jser-stat/data/posts"),
-                require("jser-stat/data/items")
-            ])
-        });
-    } else {
-        return Promise.all([
-            fetchURL("https://jser.info/posts.json"),
-            fetchURL("https://jser.info/source-data/items.json")
-        ]);
-    }
+    return Promise.all([
+        fetchItems(),
+        fetchPosts()
+    ]);
 }
+
+let _jSerStat;
 export default function fetchStat() {
-    // APIで取ってくる方式
-    if (fetchStat._jSerStat) {
-        return Promise.resolve(fetchStat._jSerStat);
+    if (_jSerStat) {
+        return Promise.resolve(_jSerStat);
     }
-    return fetchData().then(function (results) {
-        var posts = results[0].reverse();
-        var items = results[1];
-        var jSerStat = new JSerStat(items, posts);
-        fetchStat._jSerStat = jSerStat;
+    return fetchData().then(([items, posts]) => {
+        const jSerStat = new JSerStat(items, posts);
+        _jSerStat = jSerStat;
         return jSerStat;
     });
 }
